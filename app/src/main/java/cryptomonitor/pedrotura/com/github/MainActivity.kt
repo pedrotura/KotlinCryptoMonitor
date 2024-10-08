bdb5232aed8f9c9bfa9c90b597b75be0209018b2
pedrotura <phgt132@gmail.com>
2024-10-07 22:08:39 -0300

    Removendo código da MainActivity

--- app/src/main/java/cryptomonitor/pedrotura/com/github/MainActivity.kt
+++ app/src/main/java/cryptomonitor/pedrotura/com/github/MainActivity.kt
@@ -15,29 +15,6 @@ import cryptomonitor.pedrotura.com.github.ui.theme.KotlinCryptoMonitorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            KotlinCryptoMonitorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinCryptoMonitorTheme {
-        Greeting("Android")
     }
 }
