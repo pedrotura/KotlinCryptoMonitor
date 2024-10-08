a2ee43409be6461de301ff7c2ca680ba0be13bb7
pedrotura <phgt132@gmail.com>
2024-10-07 22:26:09 -0300

    Criando o payload de resposta da API

---
+++ app/src/main/java/cryptomonitor/pedrotura/com/github/model/TickerResponse.kt
@@ -0,0 +1,13 @@
+package cryptomonitor.pedrotura.com.github.model
+class TickerResponse(
+    val ticker: Ticker
+)
+class Ticker(
+    val high: String,
+    val low: String,
+    val vol: String,
+    val last: String,
+    val buy: String,
+    val sell: String,
+    val date: Long
+)
